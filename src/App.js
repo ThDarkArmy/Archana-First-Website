976f8be265a37a1b04c187493a42af1779912144
Prashant <thepenseur1729@gmail.com>
2024-07-17 07:34:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Google.com
         </a>
       </header>
     </div>
